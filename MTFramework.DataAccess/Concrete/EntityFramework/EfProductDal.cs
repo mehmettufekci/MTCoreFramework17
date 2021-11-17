3d0c6052dff7701000f94f9f6560b35d3d25c61d
mtufekci1446 <mtufekci1446@gmail.com>
2021-11-17 23:53:14 +0300

    MTFramework17 Backend initial commit

---
+++ MTFramework.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,15 @@
+﻿using MTFramework.Core.DataAccess.EntityFramework;
+using MTFramework.DataAccess.Abstract;
+using MTFramework.DataAccess.Concrete.EntityFramework.Context;
+using MTFramework.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTFramework.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+
+    }
+}
---
+++ MTFramework.WebAPI/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MTFramework.Business.DependencyResolvers.Autofac;
+
+namespace MTFramework.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // In ASP.NET Core 3.0 env will be an IWebHostEnvironment , not IHostingEnvironment.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public ILifetimeScope AutofacContainer { get; private set; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddOptions();
+
+            var builder = new ContainerBuilder();
+
+            builder.Populate(services);
+
+            builder.RegisterModule(new AutofacBusinessModule());
+
+            AutofacContainer = builder.Build();
+
+            return new AutofacServiceProvider(AutofacContainer);
+        }
+
+        public void Configure(
+          IApplicationBuilder app,
+          ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseMvc();
+        }
+    }
+}
